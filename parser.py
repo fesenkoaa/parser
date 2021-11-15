738373c52bc0c8689393e00ccf7b4f36c1ac53d1
fesenkoaa <fesenko.a@icloud.com>
2021-11-15 19:48:53 +0100

    oop

--- parser.py
+++ parser.py
@@ -4,68 +4,66 @@ import csv
 
 from config import accept, user_agent
 
-HOST = 'https://www.otomoto.pl/'
-URL = 'https://www.otomoto.pl/osobowe/bmw/m5/'
-HEADERS = {
-    'accept': accept,
-    'user-agent': user_agent
-}
-FILE = 'cars.csv'
 
+class ParserOtomoto():
 
-def get_html(url, params=None):
-    r = requests.get(url, headers=HEADERS, params=params)
-    return r
+    def __init__(self, page_url, headers, filename):
+        self.page_url = page_url
+        self.headers = headers
+        self.filename = filename
 
+    def get_html(self, url, params=None):
+        r = requests.get(self.page_url, headers=self.headers, params=params)
+        return r
 
-def get_content(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    items = soup.find_all('div', class_="offer-item__wrapper")
+    def get_content(self, html):
+        soup = BeautifulSoup(html, 'html.parser')
+        items = soup.find_all('div', class_="offer-item__wrapper")
 
-    cars = []
-    for item in items:
-        cars.append({
-            'title': item.find('a', class_='offer-title__link').get_text('title', strip=True),
-            'year': item.find('li', attrs={'data-code': 'year'}).get_text(strip=True),
-            'mileage': item.find('li', attrs={'data-code': 'mileage'}).get_text(strip=True),
-            'price': item.find('span', class_='offer-price__number ds-price-number').get_text(strip=True),
-            'city': item.find('span', class_='ds-location-city').get_text(strip=True),
-            'link': item.find('a', class_='offer-title__link').get('href')
-        })
-    return cars
-
-
-def get_pages(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    pagination = soup.find_all('span', class_='page')
-    if pagination:
-        return int(pagination[-1].get_text())
-    else:
-        return 1
-
-
-def save(items, path):
-    with open(path, 'w', newline='') as file:
-        writer = csv.writer(file, delimiter=';')
-        writer.writerow(['title', 'year', 'mileage', 'price', 'city', 'link'])
-        for item in items:
-            writer.writerow([item['title'], item['year'], item['mileage'], item['price'], item['city'], item['link']])
-
-
-def parser():
-    html = get_html(URL)
-
-    if html.ok:
         cars = []
-        pages = get_pages(html.text)
-        for page in range(1, pages + 1):
-            print(f'Parsing of page {page} of {pages}...')
-            html = get_html(URL, params={'page': page}).text
-            cars.extend(get_content(html))
-        save(cars, path=FILE)
-        print(f'Got {len(cars)}')
-    else:
-        print(html.status_code)
+        for item in items:
+            cars.append({
+                'title': item.find('a', class_='offer-title__link').get_text('title', strip=True),
+                'year': item.find('li', attrs={'data-code': 'year'}).get_text(strip=True),
+                'mileage': item.find('li', attrs={'data-code': 'mileage'}).get_text(strip=True),
+                'price': item.find('span', class_='offer-price__number ds-price-number').get_text(strip=True),
+                'city': item.find('span', class_='ds-location-city').get_text(strip=True),
+                'link': item.find('a', class_='offer-title__link').get('href')
+            })
+        return cars
+
+    def get_pages(self, html):
+        soup = BeautifulSoup(html, 'html.parser')
+        pagination = soup.find_all('span', class_='page')
+        if pagination:
+            return int(pagination[-1].get_text())
+        else:
+            return 1
+
+    def save(self, items, path):
+        with open(path, 'w', newline='') as file:
+            writer = csv.writer(file, delimiter=';')
+            writer.writerow(['title', 'year', 'mileage', 'price', 'city', 'link'])
+            for item in items:
+                writer.writerow(
+                    [item['title'], item['year'], item['mileage'], item['price'], item['city'], item['link']])
+
+    def parser(self):
+        html = self.get_html(self.page_url)
+
+        if html.ok:
+            cars = []
+            pages = self.get_pages(html.text)
+            for page in range(1, pages + 1):
+                print(f'Parsing of page {page} of {pages}...')
+                html = self.get_html(self.page_url, params={'page': page}).text
+                cars.extend(self.get_content(html))
+            self.save(cars, path=self.filename)
+            print(f'Got {len(cars)}')
+        else:
+            print(html.status_code)
 
 
-parser()
+# m5 = ParserOtomoto('https://www.otomoto.pl/osobowe/bmw/m5/', {'accept': accept, 'user-agent': user_agent}, 'cars.csv')
+# m5.parser()
+
